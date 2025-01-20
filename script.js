2b740e3ca830b0b3fb628f198b6b409cf5f90ab4
kyawnaing11 <161630204+kyawnaing11@users.noreply.github.com>
2025-01-20 10:15:05 +0100

    Update and rename Script.js to script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById('fetchData').addEventListener('click', function() {
+         fetch('https://api.smmgen.com/endpoint', {
+             method: 'GET',
+             headers: {
+                 'Authorization': 'Bearer YOUR_API_KEY', // 097f5271429fa4c0401c36b09a426cc8
+                 'Content-Type': 'application/json'
+             }
+         })
+         .then(response => response.json())
+         .then(data => {
+             document.getElementById('apiResponse').innerText = JSON.stringify(data, null, 2);
+         })
+         .catch(error => {
+             console.error('Error:', error);
+         });
+     });
