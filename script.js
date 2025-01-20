9c1ce3092ca505ac907b841f4d637fb78b12c4e9
kyawnaing11 <161630204+kyawnaing11@users.noreply.github.com>
2025-01-20 13:03:24 +0100

    Update script.js
    Keep learning

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('fetchData').addEventListener('click', function() {
-         fetch('https://api.smmgen.com/endpoint', {
+         fetch('https://smmgen.com/api/v2', {
              method: 'GET',
              headers: {
                  'Authorization': 'Bearer YOUR_API_KEY', // 097f5271429fa4c0401c36b09a426cc8
